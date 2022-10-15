7ff877992dadc60f4af19e299d815c2f60ce6951
DerpyTheSpy <erickim334@gmail.com>
2022-10-15 19:06:19 -0400

    just some changes made

--- models/NotesModel.js
+++ models/NotesModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 //TODO - Create Note Schema here having fields
 //      - noteTitle
 //      - noteDescription
@@ -7,39 +6,32 @@ const mongoose = require('mongoose');
 //      - dateAdded
 //      - dateUpdated
 
-//create noteTitle schema here
-const noteTitle = new mongoose.Schema({
+const NoteSchema = mongoose.Schema({
     noteTitle: {
         type: String,
         required: true
-    }
-});
-//create noteDescription schema here
-const noteDescription = new mongoose.Schema({
+    },
     noteDescription: {
         type: String,
         required: true
-    }
-});
-//create priority schema here. value can be high, low or medium
-const priority = new mongoose.Schema({
+    },
     priority: {
         type: String,
         required: true,
         enum: ['HIGH', 'LOW', 'MEDIUM']
-    }
-});
-//create dateAdded schema here
-const dateAdded = new mongoose.Schema({
+    },
     dateAdded: {
         type: Date,
         required: true
-    }
-});
-//create dateUpdated schema here
-const dateUpdated = new mongoose.Schema({
+    },
     dateUpdated: {
         type: Date,
         required: true
     }
+}, {
+    timestamps: true
 });
+
+
+
+module.exports = mongoose.model('Note', NoteSchema);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const DB_URL = "mongodb+srv://Test_01:1234@cluster0.ojgts9v.mongodb.net/NotesModel?retryWrites=true&w=majority"
+const DB_URL = "mongodb+srv://derpythespy:1234@cluster0.4dp6azc.mongodb.net/?retryWrites=true&w=majority"
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
