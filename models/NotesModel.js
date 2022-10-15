8c6a3212675afbd1b6987b8bd0603cce72c99694
DerpyTheSpy <erickim334@gmail.com>
2022-10-15 18:48:08 -0400

    edited notemodel and noteroutes

--- models/NotesModel.js
+++ models/NotesModel.js
@@ -6,3 +6,40 @@ const mongoose = require('mongoose');
 //      - priority (Value can be HIGH, LOW or MEDUIM)
 //      - dateAdded
 //      - dateUpdated
+
+//create noteTitle schema here
+const noteTitle = new mongoose.Schema({
+    noteTitle: {
+        type: String,
+        required: true
+    }
+});
+//create noteDescription schema here
+const noteDescription = new mongoose.Schema({
+    noteDescription: {
+        type: String,
+        required: true
+    }
+});
+//create priority schema here. value can be high, low or medium
+const priority = new mongoose.Schema({
+    priority: {
+        type: String,
+        required: true,
+        enum: ['HIGH', 'LOW', 'MEDIUM']
+    }
+});
+//create dateAdded schema here
+const dateAdded = new mongoose.Schema({
+    dateAdded: {
+        type: Date,
+        required: true
+    }
+});
+//create dateUpdated schema here
+const dateUpdated = new mongoose.Schema({
+    dateUpdated: {
+        type: Date,
+        required: true
+    }
+});
--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -9,6 +9,14 @@ app.post('/notes', (req, res) => {
         });
     }
     //TODO - Write your code here to save the note
+    // Create a Note
+    const note = new noteModel({
+        noteTitle: req.body.noteTitle || "Untitled Note",
+        noteDescription: req.body.noteDescription,
+        priority: req.body.priority,
+        dateAdded: req.body.dateAdded,
+        dateUpdated: req.body.dateUpdated
+    });
 });
 
 //TODO - Retrieve all Notes
@@ -21,6 +29,16 @@ app.get('/notes', (req, res) => {
         });
     }
     //TODO - Write your code here to returns all note
+    noteModel.find()
+    .then(notes => {
+        res.send(notes);
+    }
+    ).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    }
+    );
 });
 
 //TODO - Retrieve a single Note with noteId
@@ -33,6 +51,26 @@ app.get('/notes/:noteId', (req, res) => {
         });
     }
     //TODO - Write your code here to return onlt one note using noteid
+    noteModel.findById(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });            
+        }
+        res.send(note);
+    }
+    ).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        });
+    }
+    );
 });
 
 //TODO - Update a Note with noteId
@@ -45,6 +83,32 @@ app.put('/notes/:noteId', (req, res) => {
         });
     }
     //TODO - Write your code here to update the note using noteid
+    noteModel.findByIdAndUpdate(req.params.noteId, {
+        noteTitle: req.body.noteTitle || "Untitled Note",
+        noteDescription: req.body.noteDescription,
+        priority: req.body.priority,
+        dateAdded: req.body.dateAdded,
+        dateUpdated: req.body.dateUpdated
+    }, {new: true})
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send(note);
+    }
+    ).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating note with id " + req.params.noteId
+        });
+    }
+    );
 });
 
 //TODO - Delete a Note with noteId
@@ -57,4 +121,24 @@ app.delete('/notes/:noteId', (req, res) => {
         });
     }
     //TODO - Write your code here to delete the note using noteid
+    noteModel.findByIdAndRemove(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send({message: "Note deleted successfully!"});
+    }
+    ).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete note with id " + req.params.noteId
+        });
+    }
+    );
 });
